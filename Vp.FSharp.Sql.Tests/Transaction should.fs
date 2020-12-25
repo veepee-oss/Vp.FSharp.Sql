658d472db84c3e93a28ad9d02cb5f906ca5b65ec
Kerry Perret <kerry.perret@outlook.com>
2020-12-25 23:03:51 +0100

    feat(loggers): xmas commit

---
+++ Vp.FSharp.Sql.Tests/Transaction should.fs
@@ -0,0 +1,2 @@
+﻿module Vp.FSharp.Sql.Tests.``Transaction should``
+
--- Vp.FSharp.Sql/TransactionScope.fs
+++ Vp.FSharp.Sql/TransactionScope.fs
@@ -19,33 +19,117 @@ let DefaultTimeoutInSeconds = 30.
 
 let defaultTimeout = TimeSpan.FromSeconds(DefaultTimeoutInSeconds)
 
-let private execute isolationLevel timeout scopeOption (connection: #DbConnection) apply =
+let private newTransactionScope isolationLevel timeout scopeOption =
+    let mutable transactionOptions = TransactionOptions()
+    transactionOptions.Timeout <- timeout
+    transactionOptions.IsolationLevel <- isolationLevel
+    new TransactionScope(scopeOption, transactionOptions, TransactionScopeAsyncFlowOption.Enabled)
+
+let private execute isolationLevel timeout scopeOption
+    (connection: #DbConnection) body =
     async {
-        let mutable transactionOptions = TransactionOptions()
-        transactionOptions.Timeout <- timeout
-        transactionOptions.IsolationLevel <- isolationLevel
-        use transactionScope = new TransactionScope(scopeOption, transactionOptions, TransactionScopeAsyncFlowOption.Enabled)
+        use transactionScope = newTransactionScope isolationLevel timeout scopeOption
         connection.EnlistCurrentTransaction()
-        let! applyOutcome = apply connection
+        let! applyOutcome = body connection
         return (transactionScope, applyOutcome)
     }
 
-let complete isolationLevel timeout scopeOption (connection: #DbConnection) apply =
+let private execute2 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) body =
     async {
-        let! (transactionScope, applyOutcome) = execute isolationLevel timeout scopeOption connection apply
+        use transactionScope = newTransactionScope isolationLevel timeout scopeOption
+        connection1.EnlistCurrentTransaction()
+        connection2.EnlistCurrentTransaction()
+        let! applyOutcome = body connection1 connection2
+        return (transactionScope, applyOutcome)
+    }
+
+let private execute3 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) (connection3: #DbConnection) body =
+    async {
+        use transactionScope = newTransactionScope isolationLevel timeout scopeOption
+        connection1.EnlistCurrentTransaction()
+        connection2.EnlistCurrentTransaction()
+        connection3.EnlistCurrentTransaction()
+        let! applyOutcome = body connection1 connection2 connection3
+        return (transactionScope, applyOutcome)
+    }
+
+let complete isolationLevel timeout scopeOption (connection: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute isolationLevel timeout scopeOption connection body
+        transactionScope.Complete()
+        return applyOutcome
+    }
+let complete2 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute2 isolationLevel timeout scopeOption connection1 connection2 body
+        transactionScope.Complete()
+        return applyOutcome
+    }
+let complete3 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) (connection3: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute3 isolationLevel timeout scopeOption connection1 connection2 connection3 body
         transactionScope.Complete()
         return applyOutcome
     }
 
-let notComplete isolationLevel timeout scopeOption (connection: #DbConnection) apply =
+let notComplete isolationLevel timeout scopeOption
+    (connection: #DbConnection) body =
     async {
-        let! (_, applyOutcome) = execute isolationLevel timeout scopeOption connection apply
+        let! (_, applyOutcome) =
+            execute isolationLevel timeout scopeOption connection body
+        return! applyOutcome
+    }
+let notComplete2 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) body =
+    async {
+        let! (_, applyOutcome) =
+            execute2 isolationLevel timeout scopeOption connection1 connection2 body
+        return! applyOutcome
+    }
+let notComplete3 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) (connection3: #DbConnection) body =
+    async {
+        let! (_, applyOutcome) =
+            execute3 isolationLevel timeout scopeOption connection1 connection2 connection3 body
         return! applyOutcome
     }
 
-let completeOnSome isolationLevel timeout scopeOption (connection: #DbConnection) apply =
+let completeOnSome isolationLevel timeout scopeOption
+    (connection: #DbConnection) body =
     async {
-        let! (transactionScope, applyOutcome) = execute isolationLevel timeout scopeOption connection apply
+        let! (transactionScope, applyOutcome) =
+            execute isolationLevel timeout scopeOption connection body
+        match applyOutcome with
+        | Some some ->
+            transactionScope.Complete()
+            return Some some
+        | None ->
+            return None
+    }
+let completeOnSome2 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute2 isolationLevel timeout scopeOption connection1 connection2 body
+        match applyOutcome with
+        | Some some ->
+            transactionScope.Complete()
+            return Some some
+        | None ->
+            return None
+    }
+let completeOnSome3 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) (connection3: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute3 isolationLevel timeout scopeOption connection1 connection2 connection3 body
         match applyOutcome with
         | Some some ->
             transactionScope.Complete()
@@ -54,9 +138,35 @@ let completeOnSome isolationLevel timeout scopeOption (connection: #DbConnection
             return None
     }
 
-let completeOnOk isolationLevel timeout scopeOption (connection: #DbConnection) apply =
+let completeOnOk isolationLevel timeout scopeOption
+    (connection: #DbConnection) body =
     async {
-        let! (transactionScope, applyOutcome) = execute isolationLevel timeout scopeOption connection apply
+        let! (transactionScope, applyOutcome) =
+            execute isolationLevel timeout scopeOption connection body
+        match applyOutcome with
+        | Ok ok ->
+            transactionScope.Complete()
+            return Ok ok
+        | Error error ->
+            return Error error
+    }
+let completeOnOk2 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute2 isolationLevel timeout scopeOption connection1 connection2 body
+        match applyOutcome with
+        | Ok ok ->
+            transactionScope.Complete()
+            return Ok ok
+        | Error error ->
+            return Error error
+    }
+let completeOnOk3 isolationLevel timeout scopeOption
+    (connection1: #DbConnection) (connection2: #DbConnection) (connection3: #DbConnection) body =
+    async {
+        let! (transactionScope, applyOutcome) =
+            execute3 isolationLevel timeout scopeOption connection1 connection2 connection3 body
         match applyOutcome with
         | Ok ok ->
             transactionScope.Complete()
@@ -65,18 +175,42 @@ let completeOnOk isolationLevel timeout scopeOption (connection: #DbConnection) 
             return Error error
     }
 
-let defaultComplete transaction =
-    transaction
+let defaultComplete body =
+    body
     |> complete DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultComplete2 body =
+    body
+    |> complete2 DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultComplete3 body =
+    body
+    |> complete3 DefaultIsolationLevel defaultTimeout DefaultScopeOption
 
-let defaultNotComplete transaction =
-    transaction
+let defaultNotComplete body =
+    body
     |> notComplete DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultNotComplete2 body =
+    body
+    |> notComplete2 DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultNotComplete3 body =
+    body
+    |> notComplete3 DefaultIsolationLevel defaultTimeout DefaultScopeOption
 
-let defaultCompleteOnSome transaction =
-    transaction
+let defaultCompleteOnSome body =
+    body
     |> completeOnSome DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultCompleteOnSome2 body =
+    body
+    |> completeOnSome2 DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultCompleteOnSome3 body =
+    body
+    |> completeOnSome3 DefaultIsolationLevel defaultTimeout DefaultScopeOption
 
-let defaultCompleteOnOk transaction =
-    transaction
+let defaultCompleteOnOk body =
+    body
     |> completeOnOk DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultCompleteOnOk2 body =
+    body
+    |> completeOnOk2 DefaultIsolationLevel defaultTimeout DefaultScopeOption
+let defaultCompleteOnOk3 body =
+    body
+    |> completeOnOk3 DefaultIsolationLevel defaultTimeout DefaultScopeOption
