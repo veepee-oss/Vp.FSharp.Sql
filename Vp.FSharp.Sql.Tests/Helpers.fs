f152ad3fb6b60aedc80f0fd0696e764667bf7d91
Natalie Perret <natalie.perret.1986@gmail.com>
2021-01-06 08:18:48 +0100

    feat(tests): refactoring wip

---
+++ Vp.FSharp.Sql.Tests/Helpers.fs
@@ -0,0 +1,101 @@
+﻿module Vp.FSharp.Sql.Tests.Helpers
+
+open Swensen.Unquote.Assertions
+
+open Vp.FSharp.Sql
+
+
+type FullCallCounter =
+    { mutable OpenCall: int32
+      mutable CloseCall: int32
+      mutable ConnectionOpened: int32
+      mutable ConnectionClosed: int32
+      mutable CommandPrepared: int32
+      mutable CommandExecuted: int32 }
+
+[<RequireQualifiedAccess>]
+module FullCallCounter =
+
+    let incrOpenCall (callCounter: FullCallCounter) =
+        callCounter.OpenCall <- callCounter.OpenCall + 1
+
+    let incrCloseCall (callCounter: FullCallCounter) =
+        callCounter.CloseCall <- callCounter.CloseCall + 1
+
+    let incrConnectionOpened (callCounter: FullCallCounter) =
+        callCounter.ConnectionOpened <- callCounter.ConnectionOpened + 1
+
+    let incrConnectionClosed (callCounter: FullCallCounter) =
+        callCounter.ConnectionClosed <- callCounter.ConnectionClosed + 1
+
+    let incrCommandPrepared (callCounter: FullCallCounter) =
+        callCounter.CommandPrepared <- callCounter.CommandPrepared + 1
+
+    let incrCommandExecuted (callCounter: FullCallCounter) =
+        callCounter.CommandExecuted <- callCounter.CommandExecuted + 1
+
+    let createLoggerCallback callCounter =
+        function
+        | ConnectionOpened _ -> incrConnectionOpened callCounter
+        | ConnectionClosed _ -> incrConnectionClosed callCounter
+        | CommandPrepared _ -> incrCommandPrepared callCounter
+        | CommandExecuted _ -> incrCommandExecuted callCounter
+    let createOpenCallback (callCounter: FullCallCounter) = fun() -> incrOpenCall callCounter
+    let createCloseCallback (callCounter: FullCallCounter) = fun() -> incrCloseCall callCounter
+    let createCallbacks (callCounter: FullCallCounter) =
+        (createOpenCallback callCounter, createCloseCallback callCounter, createLoggerCallback callCounter)
+
+    let init openCall closeCall connectionOpened connectionClosed commandPrepared commandExecuted =
+        { OpenCall = openCall
+          CloseCall = closeCall
+          ConnectionOpened = connectionOpened
+          ConnectionClosed = connectionClosed
+          CommandPrepared = commandPrepared
+          CommandExecuted = commandExecuted }
+
+    let initSame value = init value value value value value value
+
+    let assertEqual
+        (actual: FullCallCounter)
+        expectedOpenCall
+        expectedCloseCall
+        expectedConnectionOpened
+        expectedConnectionClosed
+        expectedCommandPrepared
+        expectedCommandExecuted =
+        actual.OpenCall =! expectedOpenCall
+        actual.CloseCall =! expectedCloseCall
+        actual.ConnectionOpened =! expectedConnectionOpened
+        actual.ConnectionClosed =! expectedConnectionClosed
+        actual.CommandPrepared =! expectedCommandPrepared
+        actual.CommandExecuted =! expectedCommandExecuted
+
+type PartialCallCounter =
+    { mutable OpenCall: int32
+      mutable CloseCall: int32 }
+
+[<RequireQualifiedAccess>]
+module PartialCallCounter =
+    let incrOpenCall (callCounter: PartialCallCounter) =
+        callCounter.OpenCall <- callCounter.OpenCall + 1
+
+    let incrCloseCall (callCounter: PartialCallCounter) =
+        callCounter.CloseCall <- callCounter.CloseCall + 1
+
+    let createOpenCallback (callCounter: PartialCallCounter) = fun() -> incrOpenCall callCounter
+    let createCloseCallback (callCounter: PartialCallCounter) = fun() -> incrCloseCall callCounter
+    let createCallbacks (callCounter: PartialCallCounter) =
+        (createOpenCallback callCounter, createCloseCallback callCounter)
+
+    let init openCall closeCall =
+        { OpenCall = openCall
+          CloseCall = closeCall }
+
+    let initSame value = init value value
+
+    let assertEqual
+        (actual: PartialCallCounter)
+        expectedOpenCall
+        expectedCloseCall =
+        actual.OpenCall =! expectedOpenCall
+        actual.CloseCall =! expectedCloseCall
--- Vp.FSharp.Sql.Tests/SqlCommand for executeNonQuery should.fs
+++ Vp.FSharp.Sql.Tests/SqlCommand for executeNonQuery should.fs
@@ -1,112 +1,83 @@
 module Vp.FSharp.Sql.Tests.``SqlCommand for executeNonQuery should``
 
 open System.Data
+
 open Swensen.Unquote
+
 open Xunit
 
 open Vp.FSharp.Sql
+open Vp.FSharp.Sql.Tests.Helpers
 
 
 [<Fact>]
-let ``executeNonQuery should open and close the connection when it's closed`` () =
-    let openCall = ref 0
-    let closeCall = ref 0
-    let openCallback () = incr openCall
-    let closeCallback () = incr closeCall
-
+let ``open and then close the connection if initially closed`` () =
+    let callCounter = PartialCallCounter.initSame 0
+    let (openCallback, closeCallback) = PartialCallCounter.createCallbacks callCounter
     async {
         use connection =
             Mocks.NonQuery 0
             |> Mocks.makeConnection "toto" ConnectionState.Closed openCallback closeCallback
-        let! r = SqlCommand.text "update"
-                |> SqlCommand.noLogger
-                |> SqlCommand.executeNonQuery connection (Mocks.makeDependencies None None)
+        let! r =
+            SqlCommand.text "update"
+            |> SqlCommand.noLogger
+            |> SqlCommand.executeNonQuery connection (Mocks.makeDependencies None None)
         r =! 0
-        !openCall =! 1
-        !closeCall =! 1
+        PartialCallCounter.assertEqual callCounter 1 1
     }
 
 [<Fact>]
-let ``executeNonQuery should let the connection when it's other than closed`` () =
-    let openCall = ref 0
-    let closeCall = ref 0
-    let openCallback () = incr openCall
-    let closeCallback () = incr closeCall
-
+let ``leave the connection open if initially open`` () =
+    let callCounter = PartialCallCounter.initSame 0
+    let (openCallback, closeCallback) = PartialCallCounter.createCallbacks callCounter
     async {
         use connection =
             Mocks.NonQuery 1
             |> Mocks.makeConnection "toto" ConnectionState.Connecting openCallback closeCallback
-        let! r = SqlCommand.text "update"
-                |> SqlCommand.noLogger
-                |> SqlCommand.executeNonQuery connection (Mocks.makeDependencies None None)
+        let! r =
+            SqlCommand.text "update"
+            |> SqlCommand.noLogger
+            |> SqlCommand.executeNonQuery connection (Mocks.makeDependencies None None)
         r =! 1
-        !openCall =! 0
-        !closeCall =! 0
+        PartialCallCounter.assertEqual callCounter 0 0
     }
 
 [<Fact>]
-let ``executeNonQuery should log for all events on globalLogger when the connection is closed`` () =
-    let openCall = ref 0
-    let closeCall = ref 0
-    let openCallback () = incr openCall
-    let closeCallback () = incr closeCall
-    let connectionOpened = ref 0
-    let connectionClosed = ref 0
-    let commandPrepared = ref 0
-    let commandExecuted = ref 0
-    let loggerCallback =
-        function
-        | ConnectionOpened _ -> incr connectionOpened
-        | ConnectionClosed _ -> incr connectionClosed
-        | CommandPrepared _ -> incr commandPrepared
-        | CommandExecuted _ -> incr commandExecuted
+let ``log for all events on globalLogger if connection initially closed`` () =
+    let callCounter = FullCallCounter.initSame 0
+    let openCallback = FullCallCounter.createOpenCallback callCounter
+    let closeCallback = FullCallCounter.createCloseCallback callCounter
+    let loggerCallback = FullCallCounter.createLoggerCallback callCounter
     async {
         use connection =
             Mocks.NonQuery 2
             |> Mocks.makeConnection "toto" ConnectionState.Closed openCallback closeCallback
-        let deps = Some loggerCallback
-                   |> Mocks.makeDependencies None
-        let! r = SqlCommand.text "update"
-                |> SqlCommand.executeNonQuery connection deps
+        let deps =
+            Some loggerCallback
+            |> Mocks.makeDependencies None
+        let! r =
+            SqlCommand.text "update"
+            |> SqlCommand.executeNonQuery connection deps
         r =! 2
-        !openCall =! 1
-        !closeCall =! 1
-        !connectionOpened =! 1
-        !connectionClosed =! 1
-        !commandPrepared =! 1
-        !commandExecuted =! 1
+        FullCallCounter.assertEqual callCounter 1 1 1 1 1 1
     }
 
 [<Fact>]
-let ``executeNonQuery should log for just command events on globalLogger when the connection is NOT closed`` () =
-    let openCall = ref 0
-    let closeCall = ref 0
-    let openCallback () = incr openCall
-    let closeCallback () = incr closeCall
-    let connectionOpened = ref 0
-    let connectionClosed = ref 0
-    let commandPrepared = ref 0
-    let commandExecuted = ref 0
-    let loggerCallback =
-        function
-        | ConnectionOpened _ -> incr connectionOpened
-        | ConnectionClosed _ -> incr connectionClosed
-        | CommandPrepared _ -> incr commandPrepared
-        | CommandExecuted _ -> incr commandExecuted
+let ``log just command events on globalLogger if connection initially not closed`` () =
+    let callCounter = FullCallCounter.initSame 0
+    let openCallback = FullCallCounter.createOpenCallback callCounter
+    let closeCallback = FullCallCounter.createCloseCallback callCounter
+    let loggerCallback = FullCallCounter.createLoggerCallback callCounter
     async {
         use connection =
             Mocks.NonQuery 3
             |> Mocks.makeConnection "toto" ConnectionState.Connecting openCallback closeCallback
-        let deps = Some loggerCallback
-                   |> Mocks.makeDependencies None
-        let! r = SqlCommand.text "update"
-                |> SqlCommand.executeNonQuery connection deps
+        let deps =
+            Some loggerCallback
+            |> Mocks.makeDependencies None
+        let! r =
+            SqlCommand.text "update"
+            |> SqlCommand.executeNonQuery connection deps
         r =! 3
-        !openCall =! 0
-        !closeCall =! 0
-        !connectionOpened =! 0
-        !connectionClosed =! 0
-        !commandPrepared =! 1
-        !commandExecuted =! 1
+        FullCallCounter.assertEqual callCounter 0 0 0 0 1 1
     }
