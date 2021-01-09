391f7459e28da0f5d507edac8941e406d5cf0949
Natalie Perret <natalie.perret.1986@gmail.com>
2021-01-09 21:15:17 +0100

    feat(consistency): meow
    * feat(consistency): meow

---
+++ Vp.FSharp.Sql/SqlDbValue.fs
@@ -0,0 +1,11 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.NullDbValue
+
+
+let ifNone someDbValue noneDbValue = function
+    | Some value -> someDbValue value
+    | None -> noneDbValue
+
+let ifError okDbValue errorDbValue = function
+    | Ok value -> okDbValue value
+    | Error error -> errorDbValue error
