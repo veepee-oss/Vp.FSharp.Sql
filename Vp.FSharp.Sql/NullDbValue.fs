3a04471c1a4a841e0f7484e433cab557fef06c92
Natalie Perret <natalie.perret.1986@gmail.com>
2021-03-21 04:33:00 +0100

    feat(docs): rephrase, update some code comments

---
+++ Vp.FSharp.Sql/NullDbValue.fs
@@ -0,0 +1,13 @@
+﻿[<RequireQualifiedAccess>]
+module Vp.FSharp.Sql.NullDbValue
+
+
+/// Return the given DB null value if the given option is None, otherwise the underlying wrapped in Some.
+let ifNone someDbValue noneDbValue = function
+    | Some value -> someDbValue value
+    | None -> noneDbValue
+
+/// Return the given DB null value if the given option is Error, otherwise the underlying wrapped in Ok.
+let ifError okDbValue errorDbValue = function
+    | Ok value -> okDbValue value
+    | Error error -> errorDbValue error
